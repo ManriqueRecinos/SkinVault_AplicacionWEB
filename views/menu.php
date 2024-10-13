0de083b429d2d1ce610d79163d58f51b2526b53a
ManriqueR <manrique.recinso23@gmail.com>
2024-10-13 11:14:41 -0600

    Mejora de rutas

--- views/menu.php
+++ views/menu.php
@@ -1,18 +1,21 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="">SkinVault</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/skinvault/index.php">SkinVault</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link" href="index.php">Campeones</a></li>
-            <li class="nav-item"><a class="nav-link" href="">Mis Skins</a></li>
-            <li class="nav-item"><a class="nav-link" href="">Asistencia</a></li>
+        <ul class="navbar-nav">
             <li class="nav-item">
-                <form method="POST" action="controllers/AuthController.php?action=logout" class="d-inline">
-                    <button type="submit" class="btn btn-danger" style="border:none;">Cerrar Sesión</button>
-                </form>
+                <a class="nav-link" href="/skinvault/index.php?action=showChampions">Campeones</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/skinvault/index.php?action=misSkins">Mis Skins</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/skinvault/views/login.php?action=logout">Cerrar sesión</a>
             </li>
         </ul>
     </div>
 </nav>
+
+
